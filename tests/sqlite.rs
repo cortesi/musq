3de3ec9f8256c4076c6dc6d55f76ada66e35d431
Aldo Cortesi <aldo@corte.si>
2023-11-05 16:17:58 +1300

    Cargo upgrades

--- tests/sqlite.rs
+++ tests/sqlite.rs
@@ -204,27 +204,11 @@ async fn it_executes_with_pool() -> anyhow::Result<()> {
 }
 
 #[tokio::test]
-async fn it_opens_with_extension() -> anyhow::Result<()> {
-    // FIXME
-    // use std::str::FromStr;
-
-    // let opts = SqliteConnectOptions::from_str(&dotenvy::var("DATABASE_URL")?)?.extension("ipaddr");
-
-    // let mut conn = SqliteConnection::connect_with(&opts).await?;
-    // conn.execute("SELECT ipmasklen('192.168.16.12/24');")
-    //     .await?;
-    // conn.close().await?;
-
-    Ok(())
-}
-
-#[tokio::test]
 async fn it_opens_in_memory() -> anyhow::Result<()> {
     // If the filename is ":memory:", then a private, temporary in-memory database
     // is created for the connection.
     let conn = Connection::connect(":memory:").await?;
     conn.close().await?;
-
     Ok(())
 }
 
