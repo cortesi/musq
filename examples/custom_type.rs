fb6a71837251d4b9be4c81e28f797577f07905b5
Aldo Cortesi <aldo@corte.si>
2023-11-26 16:08:44 +1300

    Add an examples directory, include a custom Type implementation

---
+++ examples/custom_type.rs
@@ -0,0 +1,31 @@
+/// An example showing a complete custom Type implementation, including Encode and Decode. This is nearly identical
+/// to the code produced by the built-in Json derive.
+use musq::*;
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+struct Foo {
+    bar: String,
+}
+
+impl Type for Foo {
+    fn type_info() -> SqliteDataType {
+        SqliteDataType::Text
+    }
+    fn compatible(ty: &SqliteDataType) -> bool {
+        <&str as Type>::compatible(ty)
+    }
+}
+impl encode::Encode for Foo {
+    fn encode(self, buf: &mut ArgumentBuffer) -> encode::IsNull {
+        let v = serde_json::to_string(&self).expect("failed to encode");
+        buf.push(ArgumentValue::Text(std::sync::Arc::new(v)));
+        encode::IsNull::No
+    }
+}
+impl<'r> decode::Decode<'r> for Foo {
+    fn decode(value: &'r Value) -> Result<Self, DecodeError> {
+        serde_json::from_str(value.text()?).map_err(|x| DecodeError(x.to_string().into()))
+    }
+}
+
+fn main() {}
