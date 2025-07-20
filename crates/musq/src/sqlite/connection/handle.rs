d8e1f482dcc245a4e4c9c8a038127dc1d76c4d5a
Aldo Cortesi <aldo@corte.si>
2025-07-20 20:36:22 +1200

    Handle drop errors gracefully

--- crates/musq/src/sqlite/connection/handle.rs
+++ crates/musq/src/sqlite/connection/handle.rs
@@ -12,7 +12,10 @@ use crate::{
 /// Managed handle to the raw SQLite3 database handle.
 /// The database handle will be closed when this is dropped and no `ConnectionHandleRef`s exist.
 #[derive(Debug)]
-pub(crate) struct ConnectionHandle(NonNull<sqlite3>);
+pub(crate) struct ConnectionHandle {
+    ptr: NonNull<sqlite3>,
+    closed: bool,
+}
 
 // A SQLite3 handle is safe to send between threads, provided not more than
 // one is accessing it at the same time. This is upheld as long as [SQLITE_CONFIG_MULTITHREAD] is
@@ -27,11 +30,14 @@ unsafe impl Send for ConnectionHandle {}
 
 impl ConnectionHandle {
     pub(super) unsafe fn new(ptr: *mut sqlite3) -> Self {
-        Self(unsafe { NonNull::new_unchecked(ptr) })
+        Self {
+            ptr: unsafe { NonNull::new_unchecked(ptr) },
+            closed: false,
+        }
     }
 
     pub(crate) fn as_ptr(&self) -> *mut sqlite3 {
-        self.0.as_ptr()
+        self.ptr.as_ptr()
     }
 
     pub(crate) fn last_insert_rowid(&self) -> i64 {
@@ -60,16 +66,31 @@ impl ConnectionHandle {
             }
         }
     }
+
+    pub(crate) fn close(&mut self) -> Result<()> {
+        if self.closed {
+            return Ok(());
+        }
+        match ffi::close(self.ptr.as_ptr()) {
+            Ok(()) => {
+                self.closed = true;
+                Ok(())
+            }
+            Err(e) => Err(e.into()),
+        }
+    }
 }
 
 impl Drop for ConnectionHandle {
     fn drop(&mut self) {
         // https://sqlite.org/c3ref/close.html
-        if let Err(e) = ffi::close(self.0.as_ptr()) {
-            // This should only happen if SQLite has leaked handles internally
-            // or we misused the API. Log the error and the connection pointer
-            // so that we can troubleshoot the issue if it happens in the wild.
-            tracing::error!(db_ptr = ?self.0, "sqlite3_close failed: {}", e);
+        if !self.closed {
+            if let Err(e) = ffi::close(self.ptr.as_ptr()) {
+                // This should only happen if SQLite has leaked handles internally
+                // or we misused the API. Log the error and the connection pointer
+                // so that we can troubleshoot the issue if it happens in the wild.
+                tracing::error!(db_ptr = ?self.ptr, "sqlite3_close failed: {}", e);
+            }
         }
     }
 }
