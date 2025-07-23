f4ce359d6a7df10d4304a5fdb3190beb9d19ea54
Aldo Cortesi <aldo@corte.si>
2025-07-23 14:41:24 +1200

    Fix generic lifetime handling in FromRow macro

--- crates/musq-macros/src/tests/from_row.rs
+++ crates/musq-macros/src/tests/from_row.rs
@@ -19,6 +19,40 @@ fn derive_tuple_struct() {
 }
 
 #[test]
+fn derive_struct_with_generics() {
+    let txt = "struct Foo<T> { a: T }";
+    let tokens = expand_derive_from_row(&parse_str(txt).unwrap()).unwrap();
+    let s = tokens.to_string();
+    assert!(s.contains("impl < 'a , T > musq :: FromRow < 'a > for Foo < T >"));
+}
+
+#[test]
+fn derive_struct_with_lifetime() {
+    let txt = "struct Foo<'r, T> { a: &'r T }";
+    let tokens = expand_derive_from_row(&parse_str(txt).unwrap()).unwrap();
+    let s = tokens.to_string();
+    assert!(s.contains("impl < 'r , T > musq :: FromRow < 'r > for Foo < 'r , T >"));
+}
+
+#[test]
+fn derive_tuple_struct_with_generics() {
+    let txt = "struct Foo<T>(T);";
+    let tokens = expand_derive_from_row(&parse_str(txt).unwrap()).unwrap();
+    let s = tokens.to_string();
+    assert!(s.contains("impl < 'a , R : musq :: Row , T > musq :: FromRow < 'a > for Foo < T >"));
+}
+
+#[test]
+fn derive_tuple_struct_with_lifetime() {
+    let txt = "struct Foo<'r, T>(&'r T);";
+    let tokens = expand_derive_from_row(&parse_str(txt).unwrap()).unwrap();
+    let s = tokens.to_string();
+    assert!(
+        s.contains("impl < 'r , R : musq :: Row , T > musq :: FromRow < 'r > for Foo < 'r , T >")
+    );
+}
+
+#[test]
 fn error_on_unit_struct() {
     let txt = "struct Foo;";
     let e = expand_derive_from_row(&parse_str(txt).unwrap());
