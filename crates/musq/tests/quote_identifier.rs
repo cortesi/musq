aab7015008efca2c4ef70b61a4c577c87ff36ce9
Aldo Cortesi <aldo@corte.si>
2025-07-24 22:32:27 +1200

    Add quote_identifier escape test

---
+++ crates/musq/tests/quote_identifier.rs
@@ -0,0 +1,27 @@
+use musq::{query, query_scalar, quote_identifier};
+use musq_test::connection;
+
+#[tokio::test]
+async fn quote_identifier_escapes_double_quotes() -> anyhow::Result<()> {
+    assert_eq!(quote_identifier("user\"name"), "\"user\"\"name\"");
+
+    let conn = connection().await?;
+    let ident = "user\"name";
+    query(&format!(
+        "CREATE TABLE {} (id INTEGER)",
+        quote_identifier(ident)
+    ))
+    .execute(&conn)
+    .await?;
+    query(&format!(
+        "INSERT INTO {} (id) VALUES (1)",
+        quote_identifier(ident)
+    ))
+    .execute(&conn)
+    .await?;
+    let val: i32 = query_scalar(&format!("SELECT id FROM {}", quote_identifier(ident)))
+        .fetch_one(&conn)
+        .await?;
+    assert_eq!(val, 1);
+    Ok(())
+}
